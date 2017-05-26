ed00cbb27d6e7ea2f1fa83a722978ba46fd23960
Uzziel Perez <uzzie>
2017-05-26 04:44:15 -0500

    First commit

--- tree.C
+++ tree.C
@@ -1,6 +1,6 @@
 {
 //example of writing a tree using a struct 
-
+//WITH LOTS OF EVENTS
 gROOT->Reset();
 
 //Define the structure to hold the variables for each branch
@@ -18,7 +18,7 @@ Photon photon2;
 Int_t Events; 
 
 //create a new ROOT file
-TFile *f = new TFile("photontree.root", "RECREATE");
+TFile *f = new TFile("tree100x.root", "RECREATE");
 //create a TTree
 TTree *tree = new TTree("T", "staff data from ascii file");
 //create branches with the information from the associated structure
@@ -31,15 +31,23 @@ tree->Branch("photon2", &photon2, "pt/F:eta:phi");
 gRandom->SetSeed();
 const Int_t kUPDATE = 500;
 
-for (Int_t i=0; i<20; i++){
+for (Int_t i=0; i<5000; i++){
+Events = i;
+photon1.pt  = gRandom->Gaus(-1,1.5);
+photon1.eta = gRandom->Gaus(-0.5,0.5);
+photon1.phi = gRandom->Uniform();
+
+photon2.pt  = gRandom->Gaus(-1,1.5);
+photon2.eta = gRandom->Gaus(-0.5,0.5);
+photon2.phi = gRandom->Uniform();
+
 tree->Fill();
 }//end of for loop filling trees 
 
 
 // check what the tree looks like
 tree->Print();
-f->Close();
 f->Write();
-
+f->Close();
 
 }
